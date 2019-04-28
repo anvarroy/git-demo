7c639817f95f93d8e9b2ddce5829a43ded40b7ed
anvarroy <anvarroy@anvarroy-VirtualBox>
2019-04-28 16:42:49 +0530

    Second update after import check

--- MyWork/src/HelloWorld.scala
+++ MyWork/src/HelloWorld.scala
@@ -1,16 +1,17 @@
 import scala.io.Source
 
 object HelloWorld {
-  def main(args: Array[String]) : Unit = {
+  def main(args: Array[String]): Unit = {
     println("Hello Anvar")
     println("Welcome to Scala thru Eclipse")
-    
+
     println("Hello Soma")
     println("Hello Sathish")
     println("Hello Pandiyan")
-    
+
     println("Hello Tony")
     println("Hello Rio")
-        
+
+    println("Testing of imported project from github to eclipse and made the change")
   }
 }
